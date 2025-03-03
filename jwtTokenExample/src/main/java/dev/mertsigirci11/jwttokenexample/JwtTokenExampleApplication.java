50f09dfb06c0dc8ba08daf668e61e4d8513d6b14
Mert Sığırcı <mert_barki@hotmail.com>
2025-03-04 00:49:01 +0300

    Inıtialize Project
    To prevent from "APPLICATION FAILED TO START", datasource should be added.

---
+++ jwtTokenExample/src/main/java/dev/mertsigirci11/jwttokenexample/JwtTokenExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.mertsigirci11.jwttokenexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtTokenExampleApplication.class, args);
+    }
+
+}
---
+++ jwtTokenExample/src/test/java/dev/mertsigirci11/jwttokenexample/JwtTokenExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mertsigirci11.jwttokenexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
